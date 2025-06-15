5db3e5775fbf13cb453a9c32b2d0029d4e9dd545
Vu Tien Trinh <110036060+vutientrinh@users.noreply.github.com>
2025-06-16 00:01:21 +0700

    chagne in invoices page

--- InvoiceApp/Pages/Invoices/Index.cshtml.cs
+++ InvoiceApp/Pages/Invoices/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using InvoiceApp.Models;
+using InvoiceApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,16 @@ namespace InvoiceApp.Pages.Invoices
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContextClass context;
+        public List<Invoice> invoiceList  = new();
+
+        public IndexModel(ApplicationDbContextClass context)
+        {
+            this.context = context;
+        }
         public void OnGet()
         {
+            invoiceList = context.Invoices.OrderByDescending(i=>i.Id).ToList();
         }
     }
 }
